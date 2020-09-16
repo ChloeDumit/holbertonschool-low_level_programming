cf805565d8c66d157045ff9c7a4781f47bd7851b
Chloe <2106@holbertonschool.com>
2020-09-16 16:46:16 +0000

    adding comments and fixing betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-int main(void) {
-  
-  printf("Size of variable a : %ld byte(s)\n",sizeof(char));
-  printf("Size of int data type : %ld byte(s)\n",sizeof(int));
-  printf("Size of char data type : %ld byte(s)\n",sizeof(long int));
-  printf("Size of float data type : %ld byte(s)\n",sizeof(long long int));
-  printf("Size of double data type : %ld byte(s)\n",sizeof(float));
-  return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of variable a : %ld byte(s)\n", sizeof(char));
+printf("Size of int data type : %ld byte(s)\n", sizeof(int));
+printf("Size of char data type : %ld byte(s)\n", sizeof(long int));
+printf("Size of float data type : %ld byte(s)\n", sizeof(long long int));
+printf("Size of double data type : %ld byte(s)\n", sizeof(float));
+return (0);
 }
