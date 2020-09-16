615ffa93c418655a45936003bceb5c06d1fd3816
Chloe <2106@holbertonschool.com>
2020-09-16 13:55:03 +0000

    all done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  
+  printf("Size of variable a : %ld byte(s)\n",sizeof(char));
+  printf("Size of int data type : %ld byte(s)\n",sizeof(int));
+  printf("Size of char data type : %ld byte(s)\n",sizeof(long int));
+  printf("Size of float data type : %ld byte(s)\n",sizeof(long long int));
+  printf("Size of double data type : %ld byte(s)\n",sizeof(float));
+  return 0;
+}
