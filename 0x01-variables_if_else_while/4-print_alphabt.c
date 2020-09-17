09c12e5bfc8189f4f415a1e5aef1fd4e1c9cccd4
Chloe <2106@holbertonschool.com>
2020-09-17 17:16:37 +0000

    5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char c;
+  char e;
+  char q;
+  for (c = 'a'; c <= 'z'; c++)
+    putchar (c-e-q);
+  putchar ('\n');
+  return (0);
+}
