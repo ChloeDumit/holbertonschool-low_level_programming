810305df30fcb4329a358964f758eb4b9e769af2
Chloe <2106@holbertonschool.com>
2020-09-17 21:53:23 +0000

    fixing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ lastn = n % 10;
 /* your code goes there */
 if (lastn > 5)
 {
-printf("Last digit of %d si %d and is greater than 5\n", n, lastn);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 }
 else if (lastn == 0)
 {
