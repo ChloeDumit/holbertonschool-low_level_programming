bbf2110a914a9789a788e04b42b855a7bf0b3485
Chloe <2106@holbertonschool.com>
2020-09-17 21:48:30 +0000

    fixing betty

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+B#include <stdio.h>
 
 /**
  * main - program that prints the numbers from 00 to 99
@@ -10,25 +10,24 @@
 
 int main(void)
 {
-  int n1 = 48;
-  int n2 = 48;
-  int com = 44;
-  while (n1 <= 57)
-    {
-      n2 = 48;
-      while (n2 <= 57)
-	{
-	  putchar(n1);
-	  putchar(n2);
-	  if (n1 != 57 || n2 != 57)
-	    {
-	      putchar(com);
-	      putchar(32);
-	    }
-	  n2 += 1;
-	}
-      n1 += 1;
-    }
-  putchar('\n');
-  return (0);
+int n1 = 48;
+int n2 = 48;
+while (n1 <= 57)
+{
+n2 = 48;
+while (n2 <= 57)
+{
+putchar(n1);
+putchar(n2);
+if (n1 != 57 || n2 != 57)
+{
+putchar(44);
+putchar(32);
+}
+n2++;
+}
+n1++;
+}
+putchar('\n');
+return (0);
 }
