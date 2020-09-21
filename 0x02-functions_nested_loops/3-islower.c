b79d2c43e0b410c85334ac65cb047b9a41a3f2c9
Chloe <2106@holbertonschool.com>
2020-09-21 18:01:29 +0000

    task 5

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islower(int c)
+{
+  char r1;
+  char r0;
+  r1 = 1;
+  r0 = 0;
+if (_islower(c))
+{
+  _putchar (r1);
+ }else {
+  _putchar (r0);
+}
+ return (0);
+}
