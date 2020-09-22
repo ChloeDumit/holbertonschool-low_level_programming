57eebecd098abd2f96aac3117ae06f3928e55007
Chloe <2106@holbertonschool.com>
2020-09-22 20:48:01 +0000

    task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
-int main() {
+int main(void)
+{
 int i, t1 = 1, t2 = 2, nextTerm;
-  for (i = 1; i <= 50; ++i)
- {
-    printf("%d, ", t1);
-    nextTerm = t1 + t2;
-    t1 = t2;
-    t2 = nextTerm;
-  }
-
-  return 0;
+for (i = 1; i <= 50; ++i)
+{
+printf("%d, ", t1);
+nextTerm = t1 + t2;
+t1 = t2;
+t2 = nextTerm;
+}
+return (0);
 }
