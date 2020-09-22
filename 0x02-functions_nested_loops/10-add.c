334d855359914eacce9780b1071c376d58f5b2df
Chloe <2106@holbertonschool.com>
2020-09-22 17:17:33 +0000

    fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * add - take a sum of 2 numbers
- * @n1: first number
- * @n2: second number
- * Return: nothing
- */
+* add - take a sum of 2 numbers
+* @n1: first number
+* @n2: second number
+* Return: nothing
+*/
 int add(int a, int b)
 {
 int c = (a + b);
