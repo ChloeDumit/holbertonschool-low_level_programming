6bce40fbd0214c293df8501287a6d4d99575daef
Chloe <2106@holbertonschool.com>
2020-09-22 17:48:49 +0000

    task 8 betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,27 +7,28 @@
  */
 void jack_bauer(void)
 {
-  int h1, h2, m1, m2;
-  int tope;
-  tope = 9;
-  for (h1 = 0; h1 <= 2; h1++) 
-    {
-      for (h2 = 0; h2 <= tope; h2++)
-	{
-	  for (m1 = 0; m1 <= 5; m1++)
-	    {
-	      for (m2 = 0; m2 <= 9; m2++)
-		{
-		  if (h1==2) {
-		    tope = 3;
-		  }
-		  _putchar('0' + h1);
-		  _putchar('0' + h2);
-		  _putchar(58);		  
+int h1, h2, m1, m2;
+int tope;
+tope = 9;
+for (h1 = 0; h1 <= 2; h1++)
+{
+for (h2 = 0; h2 <= tope; h2++)
+{
+for (m1 = 0; m1 <= 5; m1++)
+{
+for (m2 = 0; m2 <= 9; m2++)
+{
+if (h1 == 2)
+{
+tope = 3;
+}
+_putchar('0' + h1);
+_putchar('0' + h2);
+_putchar(58);
 _putchar('0' + m1);
-		  _putchar('0' + m2);
-		  _putchar('\n');
-		}
+_putchar('0' + m2);
+_putchar('\n');
+}
 }
 }
 }
