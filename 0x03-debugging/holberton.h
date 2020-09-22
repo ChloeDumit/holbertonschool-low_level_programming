c2d7da2fe7bbded6588bd54843752166bab3c921
Chloe <2106@holbertonschool.com>
2020-09-22 17:22:05 +0000

    header

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,3 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
 int convert_day(int, int);
 void print_remaining_days(int, int, int);
-int largest_number(int a, int b, int c);
+int largest_number(int a, int b, int c); 
+#endif /* HOLBERTON_H */
