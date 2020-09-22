7f531080b198362a6c0c3efd08a5a2f9f8f6b455
Chloe <2106@holbertonschool.com>
2020-09-22 15:32:52 +0000

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 
   i = 0;
 
-  while (i < 10)
+  while (i < 10) /*always 0 */
     {
-      putchar(i); /*cant print numbers */
+      putchar(i);
     }
 
   printf("Infinite loop avoided! \\o/\n");
