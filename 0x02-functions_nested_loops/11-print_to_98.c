0b52eb3b849db8b790b37ea823d0c831ff9a59e0
Chloe <2106@holbertonschool.com>
2020-09-22 13:51:57 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/*
+ *
+ *
+ *
+ */
+
+void print_to_98(int n)
+{
+
+while(n <= 98)
+  {
+    printf("%d, ", n);   
+ n++;
+   
+  }
+ printf ("\n");
+}
