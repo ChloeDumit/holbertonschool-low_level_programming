7393fb421301fcce819fc947fc720770a4cbb578
Chloe <2106@holbertonschool.com>
2020-09-22 19:42:59 +0000

    s

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,11 +13,7 @@
 void print_remaining_days(int month, int day, int year)
 {
 if ((year % 4 == 0 && year % 100 == 0) || (year % 400 == 0))
-{
-if (month >= 1 && day >= 31)
-{
-  /*day++;*/
-}
+  {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
