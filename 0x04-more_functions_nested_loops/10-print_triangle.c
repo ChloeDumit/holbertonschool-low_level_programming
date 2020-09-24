604c212cbc6a917fc4cd2ecaa3ebd2bc08aaa14b
Chloe <2106@holbertonschool.com>
2020-09-24 18:17:07 +0000

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,20 +12,19 @@ int row, col;
 if (size > 0)
 {
 for (row = 1; row <= size; row++)
-  {
-    for (col = 1; col <= size; col++)
-      {
-	if (col <= size - row)
-	  _putchar(' ');
-	else 
-	  _putchar('#');
-      }
-    _putchar('\n');
-  } 
- }
- else 
-   {
-   
-    _putchar('\n');
-   }
+{
+for (col = 1; col <= size; col++)
+{
+if (col <= size - row)
+_putchar(' ');
+else 
+_putchar('#');
+}
+_putchar('\n');
+} 
+}
+else 
+{
+_putchar('\n');
+}
 }
