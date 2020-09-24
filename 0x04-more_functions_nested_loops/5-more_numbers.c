880f1865dfb0df030f4a501f244459e5a8aafa57
Chloe <2106@holbertonschool.com>
2020-09-24 17:28:03 +0000

    description

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 /**
- * print_more_numbers - print the character from 0 to 14, 10 times
+ * print_more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line.
+ *
  *Return: Always 0.
  */
 void more_numbers(void)
