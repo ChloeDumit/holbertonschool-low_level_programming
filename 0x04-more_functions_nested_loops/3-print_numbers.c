2a022ebb8f530e717aa74e7836a7d5bf6b72e115
Chloe <2106@holbertonschool.com>
2020-09-24 12:43:10 +0000

    tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_numbers -print the number
+* Return: nothing
+ */
+void print_numbers(void)
+{
+char c;
+for (c = 48; c <= 57; c++)
+_putchar(c);
+_putchar ('\n');
+}
