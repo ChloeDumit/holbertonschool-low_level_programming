5d7d3dc9878a5545c7427836803d3c6bb91f791e
Chloe <2106@holbertonschool.com>
2020-09-24 16:05:30 +0000

    fixed code

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,12 +9,19 @@ void print_square(int size)
 {
 char i;
 char j;
+if (size > 0)
+{
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
+_putchar('\n');
+}
+}
+else
+{
 _putchar('\n');
 }
 }
