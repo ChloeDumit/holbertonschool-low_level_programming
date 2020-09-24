16d65cac68d0d66d5211f5fd99ab077ca2b0d165
Chloe <2106@holbertonschool.com>
2020-09-24 17:16:46 +0000

    5 fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
  * print_more_numbers - print the character from 0 to 14, 10 times
+ *Return: Always 0.
  */
 void more_numbers(void)
 {
 int ch;
 int i;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
 for (ch = 0; ch < 15; ch++)
 {
