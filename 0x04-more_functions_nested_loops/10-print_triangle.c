d7454b2a56f4a5f80039108306c10f021ae197d9
Chloe <2106@holbertonschool.com>
2020-09-24 18:16:06 +0000

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,31 +3,29 @@
 /**
  *print_triangle -prints a triangle, followed by a new line.
  *@size - size of triangle
- *
+ *Return: Always o.
  */
 
 void print_triangle(int size)
 {
-int noOfRows, row, col1, col2, noOfCol;
-noOfRows = size;
-noOfCol = size;
+int row, col;
 if (size > 0)
 {
-for (row = 1; row <= noOfRows; row++)
-{
-for (col1 = 1; col1 <= noOfCol; col1++)
-{
-_putchar(32);
-}
-noOfCol--;
-for (col2 = 1; col2 <= row; col2++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
-else {
-_putchar('\n');
-}
+for (row = 1; row <= size; row++)
+  {
+    for (col = 1; col <= size; col++)
+      {
+	if (col <= size - row)
+	  _putchar(' ');
+	else 
+	  _putchar('#');
+      }
+    _putchar('\n');
+  } 
+ }
+ else 
+   {
+   
+    _putchar('\n');
+   }
 }
