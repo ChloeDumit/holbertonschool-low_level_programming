4a877dba13278a2659c4e6342c27aec97dee5226
Chloe <2106@holbertonschool.com>
2020-09-28 14:25:28 +0000

    tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+void print_rev(char *s)
+{
+  int temp, start, end;
+  start = 0;
+end =
+  while (start < end)
+    {
+      temp = arr[start];   
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+    }   
+}     
+ 
