3e06e40197160d3f2c17d02231e8e3c0a13c1d18
Chloe <2106@holbertonschool.com>
2020-09-29 16:52:43 +0000

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,21 @@
 #include "holberton.h"
 /**
- *
- *
+ *re_string - reverse strign
+ *@s: s
  *
  */
 void rev_string(char *s)
 {
-  int i = 0;
-  while (s[i] != '\0')
-    {
-      i++;
-    }
-  
+int i, letter, rev;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+rev = 0;
+for (i = i - 1; rev < i; i--)
+{
+letter = s[i];
+s[i] = s[rev];
+s[rev] = letter;
+rev++;
+}    
+}
