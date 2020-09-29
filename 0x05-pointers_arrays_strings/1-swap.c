186273c1472d41138b4a55433201ddecc2a17551
Chloe <2106@holbertonschool.com>
2020-09-29 11:59:40 +0000

    betty comments

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ *swap_int - swap variables
+ *@a: a
+ *@b: b
  */
 void swap_int(int *a, int *b)
 {   
- int temp = *a;
-  *a = *b;
-  *b = temp;
+int temp = *a;
+*a = *b;
+*b = temp;
 }
