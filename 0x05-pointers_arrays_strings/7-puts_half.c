5f9414e0858886e07fd19ac987cc41ce919fea0b
Chloe <2106@holbertonschool.com>
2020-09-29 18:35:56 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,15 @@ void puts_half(char *str)
 int i, lenght, mid;
 for (lenght = 0; str[lenght] != '\0'; lenght++)
 {
-}    
+}
 if (lenght % 2 != 0)
 {
 mid = ((lenght + 1) / 2);
 }
-else 
+else
 {
 mid = lenght / 2;
 }
- 
 for (i = mid; i < lenght; i++)
 {
 _putchar(str[i]);
