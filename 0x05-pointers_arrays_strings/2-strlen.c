b7e382748090788607aac0e3f9cc0687cf023e21
Chloe <2106@holbertonschool.com>
2020-09-29 12:08:48 +0000

    betty comments

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
+ *_strlen - return lenght of string
+ *@s: string
+ *Return - lenght of string
  */
 int _strlen(char *s)
 {
-  int i = 0;
-  while (s[i] != '\0')
-    {    i++;
-
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
 }
-  return (i);
+return (i);
 }
