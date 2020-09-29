29928e242e38c4d6e9ee5b6f1f5204011a811e4a
Chloe <2106@holbertonschool.com>
2020-09-29 17:17:39 +0000

    task 6

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,20 @@
 void puts_half(char *str)
 {
   
-  int c;
-  int i = 0;
-  int lenght, half1, half2;
-  while (str[i] != '\0')
-    {
-      i++;
-    }   
-  lenght = i;
-  half1 = lenght / 2;
-  half2 = lenght - half1;
-  for(c = half1; c < half2 - 1; c++)
-    {
-      _putchar(str[c]);
+int c;
+int i = 0;
+int lenght, letter;
+while (str[i] != '\0')
+{
+i++;
+}   
+lenght = i;
+ c = lenght / 2;
+for(c = (lenght - c) - 1; c > lenght / 2; c--)
+{
+letter = str[c];
+_putchar(letter);
 
-    }
+}
   _putchar('\n');
 }
