75cf34b18ba369f915dfb69dae15df9ed58f5909
Chloe <2106@holbertonschool.com>
2020-09-29 14:00:01 +0000

    try 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,22 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
+ *print_rev - print string in reverse
+ *@s: s
  *
  */
 void print_rev(char *s)
-{ 
-  int i, j, k;
-  char rev[150];
- for (i = 0; s[i] != '\0'; i++)
-   {
-     k = i;
-   }
- for (j = 0; j <= i - 1; j++)
-  {
-    rev[j] = s[k];
-    k--;
+{
+int i, j, k;
+char rev[150];
+for (i = 0; s[i] != '\0'; i++)
+{
+k = i;
 }
-    printf("%s\n", rev);
-  
+for (j = 0; j <= i - 1; j++)
+{
+rev[j] = s[k];
+k--;
+}
+printf("%s\n", rev);
 }
