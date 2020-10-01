ca1ebae597125a485977021f9ca68ebe845fa0f8
Chloe <2106@holbertonschool.com>
2020-10-01 15:40:47 +0000

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,15 +5,18 @@
  *@dest: dest
  *@src: src
  *@n: n
+ *Return: pointer to dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
- for (i = 0; i < n && src[i] != '\0'; i++) {
-   dest[i] = src[i];
- }
- while (i < n) {
-   dest[i++] = '\0';
- }
- return dest;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+while (i < n)
+{
+dest[i++] = '\0';
+}
+return (dest);
 }
