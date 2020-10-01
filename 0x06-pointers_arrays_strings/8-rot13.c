9ccbf8ee9228d894ef80a1143ac0a5365eac7f7a
Chloe <2106@holbertonschool.com>
2020-10-01 19:06:49 +0000

    task 7

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *
+ *
+ */
+char *rot13(char *)
+{
+	int i, j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
