1bd7b87843b70a8a1ccb4abf6a3b1eb6e28f1df1
Chloe <2106@holbertonschool.com>
2020-10-01 18:02:52 +0000

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*string_toupper - changes lowercase to uppercase
+*@s: s
+*Return: s
+**/
+char *string_toupper(char *s)
+	{
+		int c = 0;
+
+		while (s[c] != '\0')
+		{
+			if (s[c] >= 'a' && s[c] <= 'z')
+			{
+				s[c] = s[c] - 32;
+			}
+			c++;
+		}
+
+		return (s);
+	}
