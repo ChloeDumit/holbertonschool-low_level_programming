8fccbfc8fef95d693620fc7abcdca0edb99f4625
Chloe <2106@holbertonschool.com>
2020-10-05 17:05:21 +0000

    task 9

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,15 +1,22 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
 *
 *
 **/
 void print_diagsums(int *a, int size)
 {
-	long int d1 = 0, d2 = 0;
-	for (int i = 0; i < size; i++)
-	{
-		d1 += a[i, i];
-		d2 += a[i, n - i - 1];
-	}
-	printf("%s, %s", d1, d2);
+        int principal = 0, secondary = 0;
+
+       for (int i = 0; i < size; i++)
+{
+	for (int j = 0; j < size; j++)
+{
+	if (i == j)
+				principal = a[i][j];
+	if ((i + j) == (size - 1))
+				secondary = a[i][j];
+}
+}
+       printf("%d, %d", principal, secondary);
 }
---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *set_string - sets teh value of a pointer to a char
+ *@s: s
+ *@to:
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
