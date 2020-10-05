549a5cbb2ed35f47ba5754fa5f3e7add78f74af5
Chloe <2106@holbertonschool.com>
2020-10-05 15:11:48 +0000

    betty fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
+ *_strpbrk - searches a string for any of a set of bytes
+ *@s: s
+ *@accept: accept
+ *Return: pointer
  */
 char *_strpbrk(char *s, char *accept)
 {
 		const char *scanp;
 		int c, sc;
 
-		while ((c = *s++) != 0) {
+		while ((c = *s++) != 0)
+{
 			for (scanp = accept; (sc = *scanp++) != 0;)
 				if (sc == c)
 					return ((char *)(s - 1));
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,29 @@
 #include "holberton.h"
 /**
- *
- *
+ *_strstr - locates a substring
+ *@haystack: haystack
+ *@needle: needle
+ *Return: pointer
  **/
 char *_strstr(char *haystack, char *needle)
 {
+	unsigned int i;
+
+	i = 0;
+
+	while (haystack[i])
+	{
+	while (haystack[i] && needle[i] && haystack[i] == needle[i])
+		{
+			i++;
+		}
+
+		if (!needle[i])
+		{
+			return (haystack);
+		}
+
+		haystack++;
+	}
+	return ('\0');
+}
