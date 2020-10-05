a3d0560eb234ee383047160d12d00cbc9813dbad
Chloe <2106@holbertonschool.com>
2020-10-05 17:24:20 +0000

    last one

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,22 +1,24 @@
-#include "holberton.h"
+B#include "holberton.h"
 #include <stdio.h>
 /**
-*
-*
-**/
+ * print_diagsums - check the code for Holberton School students.
+ * @a: punt
+ * @size: punt
+ * Return: Always 0.
+ */
 void print_diagsums(int *a, int size)
 {
-        int principal = 0, secondary = 0;
+	int i = 0, j = 0, diag_one = 0, diag_two = 0;
 
-       for (int i = 0; i < size; i++)
-{
-	for (int j = 0; j < size; j++)
-{
-	if (i == j)
-				principal = a[i][j];
-	if ((i + j) == (size - 1))
-				secondary = a[i][j];
-}
-}
-       printf("%d, %d", principal, secondary);
+	for (i = 0; i < size * size; i++)
+	{
+		diag_one += *(a + i);
+		i += size;
+	}
+	for (j = 0; j < size; j++)
+	{
+		diag_two += *(a + ((size - 1) - j));
+		a += size;
+	}
+	printf("%i, %i\n", diag_one, diag_two);
 }
