e4c3b5ff53e494de218b800bc10b41da072cde5e
Chloe <2106@holbertonschool.com>
2020-10-05 16:55:03 +0000

    new uploads

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
 /**
+ *print_chessboard - print chessboard
+ *@a: array
  *
  */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
