561a508730ab346dede4c598b4c4394680ae8ca2
Chloe <2106@holbertonschool.com>
2020-10-07 15:39:03 +0000

    fixing betty !

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,14 +2,15 @@
 /**
  *_strlen_recursion - returns lenght of string
  *@s: string
+ *Return: lenght
  */
 int _strlen_recursion(char *s)
 {
 
 	if (*s == 0)
 	{
-		return(0);
+		return (0);
 	}
-	return(_strlen_recursion(s + 1) + 1);
+	return (_strlen_recursion(s + 1) + 1);
 
-	       }
+}
