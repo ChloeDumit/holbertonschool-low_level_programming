aad898e0cdf0df76c05bfa93e849d210081e30a0
Chloe <2106@holbertonschool.com>
2020-10-13 18:04:55 +0000

    betty fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,17 +8,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int saldo;
-	int monedas = 0;
-	if (argc != 2)
-	{
- printf("Error\n");
- return(1);
-	}
+int saldo;
+int monedas = 0;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 	saldo = atoi(argv[1]);
 
 	while (saldo >= 25)
-	{
+{
 		saldo = saldo - 25;
 		monedas++;
 	}
@@ -42,5 +42,5 @@ int main(int argc, char *argv[])
 		monedas++;
 	}
 	printf("%i\n", monedas);
-	return(0);
+	return (0);
 }
