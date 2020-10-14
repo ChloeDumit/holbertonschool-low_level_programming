997466091d4f9742b28d32f84de17106dcc6a221
Chloe <2106@holbertonschool.com>
2020-10-14 16:14:47 +0000

    fix2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	s = (char *) malloc((size + 1) * sizeof(char));
+	s = (char *) malloc(size * sizeof(char));
 	i = 0;
 	while (i < size)
 	{
