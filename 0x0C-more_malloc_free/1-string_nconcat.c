680132c5b1c7756089569df7856d984c7e6aff17
Chloe <2106@holbertonschool.com>
2020-10-15 15:55:51 +0000

    first attempt

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,75 +1,43 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- *_strncat - concatenates two strings
- *@dest: dest
- *@src: src
- *@n: n
- *Return: pointer to dest
- **/
-char *_strncat(char *dest, char *src, int n)
-{
-	int l, i;
+#include "holberton.h"
 
-	for (l = 0; dest[l] != '\0'; l++)
-	{
-	}
-	for (i = 0; src[i] != '\0'; i++)
-	{
-	}
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[l + i] = src[i];
-	}
-	return (dest);
-}
 /**
- *string_nconcat - Concatenates n bytes of 2 strings
- *@s1: s1
- *@s2: s2
- *@n: number of bytes
- *Return: new
+ * string_nconcat - Entry Point
+ * @s1: first string.
+ * @s2: second string.
+ * @n: spaces from s2 needed to allocate
+ *
+ * Return: nothing
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
-	char *new;
+	char *s;
+	unsigned int i, l, l1, l2, k;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-	if (n >= j)
-        {
-	new = malloc((i + j + 1) * sizeof(char));
-
-       	if (new == 0)
-	{
+	for (l1 = 0; s1[l1] != '\0'; l1++)
+		;
+	for (l2 = 0; s2[l2] != '\0'; l2++)
+		;
+	if (l2 >= n)
+		l = l1 + n + 1;
+	else
+		l = l1 + l2 + 1;
+	s = malloc(sizeof(char) * l);
+	if (s == NULL)
 		return (NULL);
-	}
-		_strncat(new, s1, i);
-		_strncat(new, s2, j);
-		new += '\0';
-	}else
-	new = malloc ((i + n + 1) * sizeof(char));
-	if (new == 0)
+	for (i = 0; i < l1; i++)
 	{
-		return(NULL);
+		s[i] = s1[i];
 	}
-	_strncat(new, s1, i);
-	_strncat(new, s2, n);
-new += '\0';
-	return (new);
-
+	for (k = 0; k + l1 + 1 < l; i++, k++)
+	{
+		s[i] = s2[k];
+	}
+	s[i] = '\0';
+	return (s);
 }
