a215b81c73a58614de7b3c3b8cda4817715bd38c
Chloe <2106@holbertonschool.com>
2020-10-15 16:28:43 +0000

    try

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,26 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
+ * *_calloc - function to allocates memory
+ * @nmemb: unsigned int type
+ * @size: unsigned int type
+ * Return: return pointer to array
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *new;
-int i;
+unsigned int i;
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 	new = malloc(nmemb * size);
-		     if (new == 0)
-		     {
-			     return (NULL);
-		     }
-		     i = 0;
-		     for (i = (i - 1); i >= 0; i--)
-		     {
-			     new[i] = '\0';
-			     
-}
-		     return(new);
+	if (new == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < nmemb * size)
+	{
+		new[i] = '\0';
+		i++;
+	}
+	return (new);
 }
