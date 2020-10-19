3ed6fda071d0a930f0b5fcf3bd234d9a985a5f25
Chloe <2106@holbertonschool.com>
2020-10-19 16:09:33 +0000

    first uploads

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
- * print_dog - prints the membders of struct dog
- * @d: pointer to struct dog
- *
- * Return: nothing
+ *new_dog - creates a new dog
+ *@name: name
+ *@age: age
+ *@owner: owner
+ *Return: new_dog
  */
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
 {
+	struct dog *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
