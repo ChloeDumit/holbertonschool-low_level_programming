955f6f4851f2149c6742bfd7dd9e9773fc00b0aa
Chloe <2106@holbertonschool.com>
2020-10-19 15:55:28 +0000

    first uploads

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints the membders of struct dog
+ * @d: pointer to struct dog
+ *
+ * Return: nothing
+ */
+dog_t *new_dog(char *name, float age, char *owner);
+{
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,3 +7,4 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _putchar(char c);
