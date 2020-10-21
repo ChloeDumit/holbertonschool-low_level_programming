6df15b1aec15f837ad2c374f2ef6c65d38869b2a
Chloe <2106@holbertonschool.com>
2020-10-21 16:17:24 +0000

    exercise 2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,4 +5,6 @@ void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 int _putchar(char c);
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
