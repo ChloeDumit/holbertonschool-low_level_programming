fb4b953af763c000211d7b602f0c3b7f8d5db80d
Chloe <2106@holbertonschool.com>
2020-10-21 15:46:14 +0000

    fisrt uploads

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 int _putchar(char c);
+#endif
