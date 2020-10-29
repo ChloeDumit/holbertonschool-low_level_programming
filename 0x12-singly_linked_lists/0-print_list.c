e15e46f20397302ea1e24b3231a489e2dfca4f23
ChloeDumit <2106@holbertonschool.com>
2020-10-29 15:32:26 +0000

    betty fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,23 +7,21 @@
  */
 size_t print_list(const list_t *h)
 {
-  unsigned int i;
-  const list_t *temp = NULL;
-  
-  i = 0;
-  temp = h;
+unsigned int i;
+const list_t *temp = NULL;
+i = 0;
+temp = h;
 
-  while (temp != NULL)
-    {
-      if (temp->str == NULL)
-	{
-	  printf("[0] (nil)\n");
-	}
-      else
-
-	  printf("[%d] %s\n", temp->len, temp->str);
-	  temp = temp->next;
-	  i++;
-    }
-  return (i);
+while (temp != NULL)
+{
+if (temp->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+printf("[%d] %s\n", temp->len, temp->str);
+temp = temp->next;
+i++;
+}
+return (i);
 }
