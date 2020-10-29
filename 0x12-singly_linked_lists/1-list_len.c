80ab61eb3186875f9b8287f17e1106ef6f902415
ChloeDumit <2106@holbertonschool.com>
2020-10-29 15:47:38 +0000

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,10 +9,12 @@ size_t list_len(const list_t *h)
 {
 	unsigned int i;
 	const list_t *temp;
+
 	i = 0;
+
 	for (temp = h; temp != NULL; temp = temp->next)
 	{
 		i++;
 	}
-	return(i);
+	return (i);
 }
