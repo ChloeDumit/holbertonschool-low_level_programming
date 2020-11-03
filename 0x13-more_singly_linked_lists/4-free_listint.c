cfca66bd5aec0bc5011ba04cc19c46456cb98861
Chloe <2106@holbertonschool.com>
2020-11-03 13:11:39 +0000

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *free_list - free list
+ *@head: head element
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *new = NULL;
+
+	while (head != NULL)
+	{
+		new = head;
+		head = head->next;
+		free(new);
+}
+}
