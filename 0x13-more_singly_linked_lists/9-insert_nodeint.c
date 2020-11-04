ae3c43d75ec88018e28831609c6aef250e408679
Chloe <2106@holbertonschool.com>
2020-11-04 12:22:41 +0000

    code fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
 	temp->n = n;
 
 	if (head != NULL && idx == 0)
