4d415db92f32787cd2dab32d0c76e9d437f76872
Chloe <2106@holbertonschool.com>
2020-11-04 12:16:56 +0000

    betty fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,14 +4,16 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * delete_nodeint_at_index - delete node at index given.
+ *@head: head
+ *@index: index
  * Return: Always 0.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int counter = 0;
 	listint_t *delete, *temp;
+
 	if (head == NULL || *head == NULL)
 	{
 		return (-1);
@@ -23,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 		delete = temp->next;
 		temp->next = delete->next;
-		free (delete);
+		free(delete);
 		return (1);
 		}
 	if (index == 0)
