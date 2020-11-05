90dc7f5e2051dc0bba0b5d590c17096f22ce6e9c
Chloe <2106@holbertonschool.com>
2020-11-05 16:31:18 +0000

    fix 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,9 +4,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	while (n != '\0')
+	if (index < sizeof(n) * 8)
 	{
+
 	*n ^= (-1 ^ *n) & (1UL << index);
+
 	return (1);
 	}
 	return (-1);
