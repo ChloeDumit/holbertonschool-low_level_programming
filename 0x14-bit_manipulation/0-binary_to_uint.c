5c8006324768104e2f42979ff833a5f0848ee968
Chloe <2106@holbertonschool.com>
2020-11-05 16:20:51 +0000

    hto fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,17 +7,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int val = 0;
-	int i = 0;
+
+	unsigned int count = 0;
+	unsigned int check = 1;
+	int x;
 
 	if (b == NULL)
-		return 0;
-
-	while (b[i] == '0' || b[i] == '1')
+		return (0);
+	x = 0;
+	while (b[x] != '\0')
 	{
-		val <<= 1;
-		val += b[i]-'0';
-		i++;
+		if (b[x] != '0' && b[x] != '1')
+		{
+			return (0);
+		}
+		count <<= 1;
+		if (b[x] == '1')
+		{
+			count ^= check;
+		}
+		x++;
 	}
-	return val;
+	return (count);
 }
