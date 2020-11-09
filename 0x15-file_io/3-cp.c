147ddf516b67558c508e5b321e7f0552100f9601
Chloe <2106@holbertonschool.com>
2020-11-09 18:14:38 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,21 +7,26 @@
  */
 int main(int argc, char *argv[])
 {
-	int file1, file2, fdwrite, fdread;
-	char buffer[1024];
+int file1, file2, fdwrite, fdread;
+char buffer[1024];
 
-	if (argc != 3)
+if (argc != 3)
+{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+}
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 < 0)
+	{
 	 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	 exit(98);
-	file2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	}
+file2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	if (file2 < 0)
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	exit(99);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 do {
 	fdread = read(file1, buffer, 1024);
 	if (fdread < 0)
