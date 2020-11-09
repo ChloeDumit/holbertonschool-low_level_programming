5f5663a2c5a6537a69a2c83133b877c6cae1cf68
Chloe <2106@holbertonschool.com>
2020-11-09 18:10:21 +0000

    betty fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,34 +1,28 @@
 #include "holberton.h"
 /**
- *append_text_to_file - read a file
- *@filename: filename
- *@text_content: letters
+ *main - main
+ *@argc: filename
+ *@argv: letters
  *Return: number of letters
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int file1, file2, fdwrite, fdread;
 	char buffer[1024];
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 < 0)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv [1]);
-       exit(98);
-	}
-	file2 = open(argv[2], O_TRUNC |O_CREAT | O_WRONLY, 0664);
+	 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	 exit(98);
+	file2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	if (file2 < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 	exit(99);
-	}
-do
-{
+
+do {
 	fdread = read(file1, buffer, 1024);
 	if (fdread < 0)
 	{
@@ -42,7 +36,6 @@ do
 		exit(99);
 	}
 } while (fdread == 1024);
-
 	if (close(file1) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
