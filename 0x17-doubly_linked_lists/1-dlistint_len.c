c77def5d63968a4488acc348e5ce1f4a498a88a5
Chloe <2106@holbertonschool.com>
2020-12-11 16:10:20 +0000

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *dlist_len - return number of elements
+ *@h: head element
+ *Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int i;
+	
+    i = 0;
+
+	while(h)
+	{
+        h = h->next;
+		i++;
+	}
+	return (i);
+}
