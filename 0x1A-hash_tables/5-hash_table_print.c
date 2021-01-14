2b0fd56d91f2d244d8e3ed203ac1a047f6b33bdd
Chloe <2106@holbertonschool.com>
2021-01-14 20:17:50 +0000

    done

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,27 +9,33 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned int i;
+    unsigned int i, j = 0;
+    
+
     if (ht == NULL)
     {
         return;
     }
-    
-    printf("{");
-    for(i = 0; i < ht->size; i++)
+    while (ht->array[j] == NULL)
     {
-
-        if(ht->array[i] != NULL)
-            printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-            if(ht->array[i]->next != NULL)
-            {
-                printf(", ");
-            }
-            else
-            {
-                printf("}\n");
-            }
-
+        j++;
     }
 
+    printf("{");
+
+    for(i = 0; i <= ht->size; i++)
+    {       
+            if(ht->array[i] != NULL)
+            {
+                if(i == j)
+                {
+                    printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value); 
+                }
+                else
+                    printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+            }
+        
+    }
+
+    printf("}\n");
 }
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function that adds an element to the hash table
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key: is the key input
+ * @value: is the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+void hash_table_delete(hash_table_t *ht);
