9047dd9470e8616dc4e7e381d34add308f95cffd
Chloe <2106@holbertonschool.com>
2021-01-15 13:04:36 +0000

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,33 +9,25 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned int i;
-    hash_node_t *tmp;
-    char *c = "";
-    
+	unsigned int i;
+	hash_node_t *tmp = NULL;
+	char *d = "";
 
-    if (ht == NULL)
-    {
-        return;
-    }
-
-    printf("{");
-
-    for(i = 0; i <= ht->size; i++)
-    {       
-            if(ht->array[i] != NULL)
-            {
-                tmp = ht->array[i];
-                while(tmp != NULL)
-                {
-                    printf("%s'%s': '%s'", c, ht->array[i]->key, ht->array[i]->value); 
-                    c = ", ";
-                    tmp = tmp->next;
-                }
-                
-            }
-        
-    }
-
-    printf("}\n");
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			tmp = ht->array[i];
+			while (tmp != NULL)
+			{
+				printf("%s\'%s\': \'%s\'", d, tmp->key, tmp->value);
+				d = ", ";
+				tmp = tmp->next;
+			}
+		}
+	}
+	printf("}\n");
 }
